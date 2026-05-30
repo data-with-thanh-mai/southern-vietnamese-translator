52e67ee0fcce91c4bbed281e82471796bed08382
data-with-thanh-mai <thaingocthanhmai@gmail.com>
2026-05-30 18:21:55 +0700

    Update data.loader.py

--- source/data/data.loader.py
+++ source/data/data.loader.py
@@ -6,14 +6,6 @@ class DialectDataset(Dataset):
     Lớp xử lý dữ liệu tùy chỉnh cho bài toán dịch phương ngữ Miền Tây sang Phổ thông.
     """
     def __init__(self, dataframe, tokenizer, max_source_len=256, max_target_len=256):
-        """
-        Khởi tạo Dataset.
-        Args:
-            dataframe (pd.DataFrame): Dữ liệu dạng bảng chứa 2 cột đã làm sạch là 'source_text' và 'target_text'.
-            tokenizer: HuggingFace Tokenizer (ví dụ: viT5 hoặc BARTPho tokenizer).
-            max_source_len (int): Độ dài tối đa cho chuỗi đầu vào.
-            max_target_len (int): Độ dài tối đa cho chuỗi đầu ra (nhãn).
-        """
         self.data = dataframe
         self.tokenizer = tokenizer
         self.max_source_len = max_source_len
@@ -46,14 +38,10 @@ class DialectDataset(Dataset):
             return_tensors='pt'
         )
 
-        # KỸ THUẬT CHE NHÃN (LABEL MASKING)
-        labels = target_encoding['input_ids'].squeeze()
-        labels[labels == self.tokenizer.pad_token_id] = -100
-
         return {
             'input_ids': source_encoding['input_ids'].squeeze(),
             'attention_mask': source_encoding['attention_mask'].squeeze(),
-            'labels': labels
+            'labels': target_encoding['input_ids'].squeeze()
         }
 
 def create_dataloader(dataframe, tokenizer, batch_size, max_source_len=256, max_target_len=256, is_train=True):
@@ -71,6 +59,6 @@ def create_dataloader(dataframe, tokenizer, batch_size, max_source_len=256, max_
         dataset,
         batch_size=batch_size,
         shuffle=is_train,       
-        num_workers=2,          # Sử dụng đa luồng để nạp dữ liệu nhanh hơn
-        pin_memory=True         # Tối ưu hóa tốc độ chuyển tensor từ CPU sang GPU
+        num_workers=2,          
+        pin_memory=True         
     )
